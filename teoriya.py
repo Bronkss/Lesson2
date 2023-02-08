38a0a828480662ced32bb36fcb5c493a315a1b41
Alex <alexandr.yakimov.heaven@gmail.com>
2023-02-08 15:36:19 +0500

    Task done!

---
+++ teoriya.py
@@ -0,0 +1,62 @@
+# a = 'hello'
+# for i in a:
+#     print(i, end='')
+
+# for e in range(0, a, 2):
+#     print(e)
+
+# for ind in range(0, len(a)):
+#     print(a[ind])
+
+
+# for _ in range(3):
+#     print("Hello")
+
+# Task 9.
+
+# n = int(input())
+#
+# if n == 0:
+#     print('Факториал 0 = 1')
+#
+# count = 1
+# result = 1
+# while count <= n:
+#     result = result * count
+#     count = count + 1
+# print(result)
+
+
+# Task 11.
+
+# n = int(input("Введите число: "))
+#
+# a,b = 0, 1
+# i = 1
+#
+# while a + b <= n:
+#     a, b = b, a + b
+#     i = i + 1
+#
+# if n == b:
+#     print(f'Позиция элемента - {i + 1}')
+# else:
+#     print(-1)
+
+# Task 13.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
